aca63e45a67ec33f03208d2b38559c5ba50ccb4a
Thorben0 <thorben.stumpe@gmail.com>
2021-12-28 21:13:18 +0100

    :sparkles: Add stock list route

--- src/main/java/com/shopicorn/storage/stock/StockController.java
+++ src/main/java/com/shopicorn/storage/stock/StockController.java
@@ -1,15 +1,18 @@
 package com.shopicorn.storage.stock;
 
+import java.util.List;
 import java.util.function.Supplier;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
-@RestController()
+@RestController
+@RequestMapping("/storage/stock")
 public class StockController {
 
 	@Autowired
@@ -18,10 +21,15 @@ public class StockController {
 	final Supplier<RuntimeException> throwNotFound = () -> new ResponseStatusException(HttpStatus.NOT_FOUND,
 			"Stock not found");
 
-	@GetMapping("/storage/stock/{id}")
+	@GetMapping("/{id}")
 	StockEntity get(@PathVariable long id) {
 		var optional = repo.findById(id);
 		var entity = optional.orElseThrow(throwNotFound);
 		return entity;
+	}
+
+	@GetMapping
+	List<StockEntity> list() {
+		return repo.findAll();
 	}
 }
