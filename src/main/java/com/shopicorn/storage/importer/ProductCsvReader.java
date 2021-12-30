05930f5121fae69abdb1838494ff009f8ddd900d
Thorben0 <thorben.stumpe@gmail.com>
2021-12-30 17:30:18 +0100

    :sparkles: Implement product svg reader

---
+++ src/main/java/com/shopicorn/storage/importer/ProductCsvReader.java
@@ -0,0 +1,40 @@
+package com.shopicorn.storage.importer;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.stream.StreamSupport;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+
+class ProductCsvReader {
+	private final File file;
+
+	public ProductCsvReader(File csvFile) {
+		this.file = csvFile;
+	}
+
+	public Collection<ProductEntity> readCsv() throws IOException {
+		try (CSVParser csvParser = new CSVParser(Files.newBufferedReader(file.toPath()), CSVFormat.DEFAULT);) {
+			Iterable<CSVRecord> csvRecords = csvParser.getRecords();
+			return StreamSupport.stream(csvRecords.spliterator(), false).map(this::toProductEntity).toList();
+		} catch (IOException e) {
+			throw new IOException("Csv import failed", e);
+		}
+	}
+
+	private ProductEntity toProductEntity(CSVRecord record) {
+		long id = Long.parseLong(record.get("id"));
+		String color = record.get("color");
+		String category = record.get("category");
+		String description = record.get("description");
+		String manufacturer = record.get("manufacturer");
+		LocalDate releaseDate = LocalDate.parse(record.get("releaseDate"));
+		int stars = Integer.parseInt(record.get("stars"));
+		return new ProductEntity(id, color, category, description, manufacturer, releaseDate, stars);
+	}
+}
--- src/main/java/com/shopicorn/storage/importer/ProductEntity.java
+++ src/main/java/com/shopicorn/storage/importer/ProductEntity.java
@@ -5,10 +5,14 @@ import java.time.LocalDate;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProductEntity {
 
 	@Id
